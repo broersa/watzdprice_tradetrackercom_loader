71b719686c7d43984072e5171ad13d1693f9fc38
Andre Broers <broersa@Andres-MacBook-Air.local>
2017-02-01 21:36:53 +0100

    service calls added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 var config = require('config');
 var main = require('./app/main.js');
 
-console.log(config.tradetrackercom_url);
-console.log(config.tradetrackercom_shop);
-console.log(config.tradetrackercom_elastic_url);
-console.log(config.tradetrackercom_elastic_index);
-
-main.process(config.tradetrackercom_url, config.tradetrackercom_elastic_url, config.tradetrackercom_elastic_index, config.tradetrackercom_shop, function (error) {
+main.process(config.tradetrackercom_url, config.tradetrackercom_watzdprice_url, config.tradetrackercom_shop, function (error) {
   if (error) {
     console.log(error);
   }
--- src/app/main.js
+++ src/app/main.js
@@ -1,34 +1,104 @@
 var async = require('async');
 var moment = require('moment');
 var tradeTrackerCom = require('../dal/tradeTrackerCom.js');
-var watzdprice = require('watzdprice');
+var url = require('url');
+var http = require('http');
 
 module.exports = {
-  process: function(url, elastic_url, elastic_index, shop, cb) {
-    tradeTrackerCom.getTradeTrackerComData(url, function(data) {
-      watzdprice.startSession(elastic_url, function (error, client) {
+  process: function(tradetracker_url, watzdprice_url, shop, cb) {
+    var start = moment().format();
+    var added = 0;
+    var updated = 0;
+    var urlDetails = url.parse(watzdprice_url);
+    tradeTrackerCom.getTradeTrackerComData(tradetracker_url, function(data) {
+      async.eachLimit(data.products, 1, function (i, cb) {
+        var product = {
+          name: i.name,
+          description: i.description,
+          eancode: i.properties.EAN[0],
+          shop: shop,
+          category: '',
+          price: i.price.amount,
+          url: i.URL,
+          image: i.images[0],
+          datetime: moment().format()
+        };
+        putProduct(urlDetails, JSON.stringify(product), function (err, operation) {
+          if (operation === 'added') {
+            added++;
+          }
+          if (operation === 'updated') {
+            updated++;
+          }
+          cb(err);
+        });
+      }, function (error) {
         if (error) {
           return cb(error);
         }
-        async.eachLimit(data.products, 1, function (i, cb) {
-          var product = {
-            name: i.name,
-            description: i.description,
-            eancode: i.properties.EAN[0],
-            shop: shop,
-            category: '',
-            price: i.price.amount,
-            url: i.URL,
-            image: i.images[0]
-          };
-          watzdprice.updateProduct(client, elastic_index, moment(), product, cb);
-        }, function (error) {
-          if (error) {
-            return cb(error);
-          }
-          return cb()
-        });
+        return postShopLoadStats(urlDetails, JSON.stringify({
+          shop: shop,
+          start: start,
+          end: moment().format(),
+          added: added,
+          updated: updated
+        }), cb);
       });
     });
   }
+}
+
+function putProduct (urlDetails, product, callback) {
+  var put_options = {
+    host: urlDetails.hostname,
+    port: urlDetails.port,
+    path: '/updateproduct',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(product)
+    }
+  };
+
+  // Set up the request
+  var put_req = http.request(put_options, function(res) {
+      res.setEncoding('utf8');
+      var body = '';
+      res.on('data', function (chunk) {
+        body = body + chunk;
+      });
+      res.on('end', function() {
+        var d = JSON.parse(body);
+        callback(null, d.operation);
+      })
+  });
+
+  // post the data
+  put_req.write(product);
+  put_req.end();
+}
+
+function postShopLoadStats (urlDetails, shopLoadStats, callback) {
+  var post_options = {
+    host: urlDetails.hostname,
+    port: urlDetails.port,
+    path: '/addshoploadstats',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(shopLoadStats)
+    }
+  };
+
+  // Set up the request
+  var put_req = http.request(post_options, function(res) {
+      res.setEncoding('utf8');
+      res.on('end', function() {
+        callback(null);
+      })
+  });
+
+  // post the data
+  put_req.write(shopLoadStats);
+  put_req.end();
 }
